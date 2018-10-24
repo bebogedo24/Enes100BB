f61742d556d689ee135354b33b6a39319f391654
Bahaa Harraz <bahaaharraz@wireless-10-104-95-30.umd.edu>
2018-10-24 11:06:10 -0400

    Created Source file and Setup Libraries

---
+++ BlackBox.ino
@@ -0,0 +1,17 @@
+#include <Enes100.h>
+#include <Enes100Simulation.h>
+#include <DFRTankSimulation.h>
+
+#define MARKER_ID 1
+#define APC220_OUT 2
+#define APC220_IN 3
+
+Enes100 enes("BlackBoxTeam", BLACK_BOX, MARKER_ID, APC220_IN, APC220_OUT);
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
