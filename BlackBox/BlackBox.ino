9c947ae8974c9111e5c41bac2b2ae1ff69110081
Bahaa Harraz <bahaaharraz@wireless-10-104-83-64.umd.edu>
2018-10-31 10:57:25 -0400

    wrote code for ms5 turning, communication, and navigation tests in simulation and outside of simulation.

--- BlackBox/BlackBox.ino
+++ BlackBox/BlackBox.ino
@@ -1,37 +1,112 @@
 #include <Enes100.h>
-#include <Enes100Simulation.h>
-#include <DFRTankSimulation.h>
+#include <DFRTank.h>
 
-#define MARKER_ID 1
-#define APC220_OUT 2
-#define APC220_IN 3
+#define MARKER_ID  13
+#define RX_PIN 8
+#define TX_PIN 9
+#define GOAL_X 2
+#define GOAL_Y 1
+#define TOLERANCE 0.05
+#define UP 1.570796
+#define DOWN -1.570796
 
-Enes100 enes("BlackBoxTeam", BLACK_BOX, MARKER_ID, APC220_IN, APC220_OUT);
+Enes100 enes("Lost", BLACK_BOX, MARKER_ID, RX_PIN, TX_PIN);
+DFRTank tank;
+
+int turn_cw(double newTheta);
+int turn_ccw(double newTheta);
 
 void setup() {
-  // put your setup code here, to run once:
+  tank.init();
+
+  while (!enes.retrieveDestination());
+
+  while (!enes.updateLocation());
+
+  enes.println("Connected and Setup");
+}
+
+//turns clockwise to theta, meant to be used in loop until 1 is returned
+int turn_cw(double newTheta){
+  delay(100);
+  enes.updateLocation();
+  if(enes.location.theta > newTheta + TOLERANCE) {
+    //turn right:
+    tank.setRightMotorPWM(-255);
+    tank.setLeftMotorPWM(255);
+    return 0;
+  } else if (enes.location.theta < newTheta - TOLERANCE) {
+    //turn left:
+    tank.setRightMotorPWM(255);
+    tank.setLeftMotorPWM(-255);
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
+
+//turns couner clockwise to theta, meant to be used in loop until 1 is returned
+int turn_ccw(double newTheta){
+  delay(100);
+  enes.updateLocation();
+  if(enes.location.theta > newTheta + TOLERANCE) {
+    //turn left:
+    tank.setRightMotorPWM(255);
+    tank.setLeftMotorPWM(-255);
+    return 0;    
+
+  } else if (enes.location.theta < newTheta - TOLERANCE) {
+    //turn right:
+    tank.setRightMotorPWM(-255);
+    tank.setLeftMotorPWM(255);
+    return 0;
+    
+  } else {
+    return 1;
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-     // Update the OSV's current location
-    if (enes.updateLocation()) {
-        enes.println("Location updated!");
-        enes.print("My x coordinate is ");
-        enes.println(enes.location.x);
-        enes.print("My y coordinate is ");
-        enes.println(enes.location.y);
-        enes.print("My theta is ");
-        enes.println(enes.location.theta);
-    } else {
-        enes.println("Couldn't update my location");
-    }
+  
+  if (enes.updateLocation()) {
+    if(enes.location.x < (GOAL_X + TOLERANCE)){
+      while(!turn_cw(0));
+      tank.setRightMotorPWM(255);
+      tank.setLeftMotorPWM(255);
+      
+    } else if(enes.location.y > (GOAL_Y + TOLERANCE)){
+      while(!turn_cw(DOWN));
+      tank.setRightMotorPWM(255);
+      tank.setLeftMotorPWM(255);      
+      
+    } else if(enes.location.y < (GOAL_Y - TOLERANCE)){
+      while(!turn_cw(UP));
+      tank.setRightMotorPWM(255);
+      tank.setLeftMotorPWM(255);  
+      
+    }else {
+      enes.println("done");
+      tank.turnOffMotors();
+      
+      enes.navigated();
+      Coordinate ret(GOAL_X, GOAL_Y);
+      enes.baseObjective(ret);
+      
+      while(!turn_cw(UP));
+      tank.turnOffMotors();
+      delay(500);
 
-    enes.navigated();
+      while(!turn_cw(0));
+      tank.turnOffMotors();
+      delay(500);
 
-    // Transmit the coordinate of the black box
-    Coordinate blackBox(2.9, 1.4);
-    enes.baseObjective(blackBox);
+      while(!turn_cw(DOWN));
+      tank.turnOffMotors();
+      delay(500);
+
+      while(!turn_cw(0));
+      tank.turnOffMotors();    
+    } 
+  }
 }
-}
-
--- Simulation Code/BlackBoxSim/BlackBoxSim.ino
+++ Simulation Code/BlackBoxSim/BlackBoxSim.ino
@@ -5,16 +5,19 @@
 #define MARKER_ID 1
 #define RX_PIN 8
 #define TX_PIN 9
-#define GOAL_X 2.5
-#define GOAL_Y 
+#define GOAL_X 2
+#define GOAL_Y 1
 #define TOLERANCE 0.05
-#define UP 1.57
-#define DOWN -1.57
+#define UP 1.570796
+#define DOWN -1.570796
 
 //To transfer to actual OSV, comment out "Enes100Simulation enes;" and uncoment:
 //Enes100 enes("Lost", BLACK_BOX, MARKER_ID, RX_PIN, TX_PIN);
 Enes100Simulation enes;
 DFRTankSimulation tank;
+
+int turn_cw(double newTheta);
+int turn_ccw(double newTheta);
 
 void setup() {
   tank.init();
@@ -25,62 +28,91 @@ void setup() {
 
   enes.println("Connected and Setup");
 }
+
+//turns clockwise to theta, meant to be used in loop until 1 is returned
+int turn_cw(double newTheta){
+  delay(100);
+  enes.updateLocation();
+  if(enes.location.theta > newTheta + TOLERANCE) {
+    //turn right:
+    tank.setRightMotorPWM(-255);
+    tank.setLeftMotorPWM(255);
+    return 0;
+  } else if (enes.location.theta < newTheta - TOLERANCE) {
+    //turn left:
+    tank.setRightMotorPWM(255);
+    tank.setLeftMotorPWM(-255);
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
+
+//turns couner clockwise to theta, meant to be used in loop until 1 is returned
+int turn_ccw(double newTheta){
+  delay(100);
+  enes.updateLocation();
+  if(enes.location.theta > newTheta + TOLERANCE) {
+    //turn left:
+    tank.setRightMotorPWM(255);
+    tank.setLeftMotorPWM(-255);
+    return 0;    
+
+  } else if (enes.location.theta < newTheta - TOLERANCE) {
+    //turn right:
+    tank.setRightMotorPWM(-255);
+    tank.setLeftMotorPWM(255);
+    return 0;
+    
+  } else {
+    return 1;
+  }
+}
+
 void loop() {
+  
   if (enes.updateLocation()) {
     if(enes.location.x < (GOAL_X + TOLERANCE)){
-      if(enes.location.theta > 0.1) {
-        //turn right:
-        tank.setRightMotorPWM(-255);
-        tank.setLeftMotorPWM(255);
-      } else if (enes.location.theta < -0.1) {
-        //turn left:
-        tank.setRightMotorPWM(255);
-        tank.setLeftMotorPWM(-255);
-      } else {
-        //move forward:
-        tank.setRightMotorPWM(255);
-        tank.setLeftMotorPWM(255);
-      }
-    } else if(enes.location.y < (GOAL_Y + TOLERANCE)){
-      //1.47 = pi/2 - 0.1
-      if(enes.location.theta > (DOWN - TOLERANCE)) {
-        //turn left:
-        tank.setRightMotorPWM(255);
-        tank.setLeftMotorPWM(-255);
-
-        //1.67 = pi/2 + 0.1
-      } else if (enes.location.theta < (DOWN + TOLERANCE)) {
-        //turn right:
-        tank.setRightMotorPWM(-255);
-        tank.setLeftMotorPWM(255);
-      } else {
-        //move forward:
-        tank.setRightMotorPWM(255);
-        tank.setLeftMotorPWM(255);
-      }
+      while(!turn_cw(0));
+      tank.setRightMotorPWM(255);
+      tank.setLeftMotorPWM(255);
+      
     } else if(enes.location.y > (GOAL_Y + TOLERANCE)){
-      //1.47 = pi/2 - 0.1
-      if(enes.location.theta > (UP - TOLERANCE)) {
-        //turn left:
-        tank.setRightMotorPWM(255);
-        tank.setLeftMotorPWM(-255);
-
-        //1.67 = pi/2 + 0.1
-      } else if (enes.location.theta < (UP + TOLERANCE)) {
-        //turn right:
-        tank.setRightMotorPWM(-255);
-        tank.setLeftMotorPWM(255);
-      } else {
-        //move forward:
-        tank.setRightMotorPWM(255);
-        tank.setLeftMotorPWM(255);
-      } 
+      while(!turn_cw(DOWN));
+      tank.setRightMotorPWM(255);
+      tank.setLeftMotorPWM(255);      
+      
+    } else if(enes.location.y < (GOAL_Y - TOLERANCE)){
+      while(!turn_cw(UP));
+      tank.setRightMotorPWM(255);
+      tank.setLeftMotorPWM(255);  
+      
     }else {
+      enes.println("done");
       tank.turnOffMotors();
       //IMPORATNT: uncomment when transferring to real OSV:
       //enes.navigated();
       //Coordinate ret(GOAL_X, GOAL_Y);
       //enes.baseObjective(ret);
+      enes.println("Before UP");
+      while(!turn_cw(UP));
+      tank.turnOffMotors();
+      delay(500);
+      enes.println("UP");
+      while(!turn_cw(0));
+      tank.turnOffMotors();
+      delay(500);
+      enes.println("0");
+      while(!turn_cw(DOWN));
+      tank.turnOffMotors();
+      delay(500);
+      enes.println("DOWN");
+      while(!turn_cw(0));
+      tank.turnOffMotors();
+      enes.println("0");
+      
+      
     }
     
   }
